d1a50968025b2915ddb025637992f6d8b275f57d
npchi <andreapozochocano@gmail.com>
2024-08-02 11:24:06 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,85 @@
+jQuery(function ($) {
+	'use strict';
+
+	/* ----------------------------------------------------------- */
+	/*  Fixed header
+	/* ----------------------------------------------------------- */
+	$(window).on('scroll', function () {
+
+		// scroll to top btn show/hide
+		function scrollTopBtn() {
+			var scrollToTop = $('#back-to-top'),
+				scroll = $(window).scrollTop();
+			if (scroll >= 50) {
+				scrollToTop.fadeIn();
+			} else {
+				scrollToTop.fadeOut();
+			}
+		}
+		scrollTopBtn();
+	});
+
+
+	$(document).ready(function () {
+
+		// back to top
+		function backToTop() {
+			$('#back-to-top').on('click', function () {
+				$('#back-to-top').tooltip('hide');
+				$('body,html').animate({
+					scrollTop: 0
+				}, 800);
+				return false;
+			});
+		}
+		backToTop();
+
+
+		// banner-carousel
+		function bannerCarouselOne() {
+			$('.banner-carousel.banner-carousel-1').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				dots: true,
+				speed: 600,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+			});
+			$('.banner-carousel.banner-carousel-1').slickAnimation();
+		}
+		bannerCarouselOne();
+
+		// pageSlider
+		function pageSlider() {
+			$('.page-slider').slick({
+				fade: true,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				dots: false,
+				speed: 600,
+				arrows: true,
+				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
+			});
+		}
+		pageSlider();
+
+		// testimonial carousel
+		function testimonialCarousel() {
+			$('.testimonial-slide').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true,
+				speed: 600,
+				arrows: false
+			});
+		}
+		testimonialCarousel();
+
+	});
+
+
+});
